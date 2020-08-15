7564bbe28cf3404bfccea7267a1fb717f61c3341
Karthi13krishna <32244868+Karthi13krishna@users.noreply.github.com>
2020-08-15 16:59:12 +0530

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 from ttkthemes import ThemedStyle
-import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
@@ -9,26 +8,23 @@ a = [0,0,0,0]
 b = ['Electricity','Bus','Local Train','Train']
 
 def button_pressed():
-    a[0] = calc(elec_entry.get(),0.708)
-    a[1] = calc(bus_entry.get(),0.10312)
-    a[2] = calc(local_train_entry.get(),0.036939994)
-    a[3] = calc(train_entry.get(),0.00497)
+    a[0] = calc(elec_entry.get(), 0.708, people_entry.get())
+    a[1] = calc(bus_entry.get(),0.10312,1)
+    a[2] = calc(local_train_entry.get(),0.036939994,1)
+    a[3] = calc(train_entry.get(),0.00497,1)
     final_text.set("{:.2f}".format(sum(a)/1000) + " metric tons of CO2")
     
-    y_label = tk.Label(tab3, text = "kg of CO2", padx = 10, pady =10)
-    y_label.grid(row = 0, column = 0)
+    y_label = tk.Label(result, text = "kg of CO2", padx = 10, pady =10)
+    y_label.grid(row = 2, column = 0)
     
     f = Figure(figsize=(4,3), dpi=100)
-    ax = f.add_subplot(111).bar(b,a)
-    
-    result_bar = FigureCanvasTkAgg(f, tab3)
-    result_bar.get_tk_widget().grid(row = 0, column = 1, padx = 30, pady = 10)
+    f.add_subplot(111).bar(b,a)
+    result_bar = FigureCanvasTkAgg(f, result)
+    result_bar.get_tk_widget().grid(row = 2, column = 1, padx = 10, pady = 10)
     
     maxi = b[a.index(max(a))]
-    result_lab = tk.Label(tab3, text = "Your " + maxi + " emission is too high!. Try to reduce it :)", padx = 30, pady = 10)
-    result_lab.grid(row = 1, columnspan = 2)
-    
-    tabControl.select(2)
+    result_text.set("Your " + maxi + " emission is too high!. Try to reduce it :)")
+    tabControl.select(3)
         
 def selec0():
     tabControl.select(0)
@@ -36,11 +32,12 @@ def selec1():
     tabControl.select(1)
 def selec2():
     tabControl.select(2)
+def selec3():
+    tabControl.select(3)   
     
-    
-def calc(var, val):
+def calc(var, val, people):
     try:
-        carbon = (float(var) * val)
+        carbon = (float(var) * val) / int(people)
     except:
         carbon = 0
     return carbon
@@ -49,58 +46,73 @@ root = tk.Tk()
 style = ThemedStyle(root)
 style.set_theme("plastik")
 root.title("CarbonFootprint Calculator")
-root.geometry("640x480")
+root.geometry("640x530")
 
 tabControl = ttk.Notebook(root)
-tab1 = ttk.Frame(tabControl) 
-tab2 = ttk.Frame(tabControl)
-tab3 = ttk.Frame(tabControl)
+house = ttk.Frame(tabControl) 
+transport = ttk.Frame(tabControl)
+result = ttk.Frame(tabControl)
+welcome = ttk.Frame(tabControl)
 
-tabControl.add(tab1, text ='House') 
-tabControl.add(tab2, text ='Transport')
-tabControl.add(tab3, text = 'Result')
+tabControl.add(welcome, text ='Welcome') 
+tabControl.add(house, text ='House') 
+tabControl.add(transport, text ='Transport')
+tabControl.add(result, text = 'Result')
 style.configure("Tab", focuscolor=style.configure(".")["background"])
 tabControl.pack(expand = 1, fill ="both")
 
-elec_label = tk.Label(tab1, text = "Electricity (in kWh):", padx = 50, pady =30)
-elec_entry = ttk.Entry(tab1)
+welcome_message = tk.Message(welcome, text = '''A carbon footprint is the measure of total greenhouse gas emissions, caused 
+by an individual or an organization, expressed as carbon dioxide equivalent.
+These greenhouse gases are released by our day-to-day activities including, 
+but not limited to, transportation, electricity usage, the dress we wear and even 
+the food we eat. We are taking buried carbon in the form of fossil fuels and 
+releasing it to the atmosphere again. An increase in greenhouse gas 
+emissions is the main cause for climate change. Many people are ignorant of 
+this and keep adding more carbon to the atmosphere. To limit temperature rise
+to 1.5°C, we must reduce our greenhouse gas emissions 7.6% each year for 
+the next decade. Each individual must know how much carbon footprint they leave and know 
+how much their activity affect the environment, so that they can be aware of 
+their actions. In this project an easy-to-use carbon footprint calculator is 
+developed. It can be used by anyone to calculate how much carbon they emit. 
+It also provides smart suggestions based on their carbon emission and help 
+them reduce their carbon footprint. Moving forward in technology is good, but 
+we also want to start taking care of our environment, smartly''').pack(padx = 10, pady = 10)
+house1_button = ttk.Button(welcome, text = "Next: House", command = selec1).pack(padx = 100, pady = 10)
 
-elec_label.grid(row=0, column=0)
-elec_entry.grid(row = 0, column=1)
+people_label = tk.Label(house, text = "No of persons in the house:", padx = 50, pady =10).grid(row=0, column=0)
+people_entry = ttk.Entry(house)
+people_entry.grid(row = 0, column=1)
 
-transport_button = ttk.Button(tab1, text = "Next: Transport", command = selec1)
-transport_button.grid(row = 1, column = 1)
+elec_label = tk.Label(house, text = "Electricity (in kWh):", padx = 50, pady =10).grid(row=1, column=0)
+elec_entry = ttk.Entry(house)
+elec_entry.grid(row = 1, column=1)
 
-bus_label = tk.Label(tab2, text = "Bus (in km):", padx = 50, pady =10)
-bus_entry = ttk.Entry(tab2)
+welcome_button = ttk.Button(house, text = "Back: Welcome", command = selec0).grid(row = 2, column = 0, pady = 10)
+transport_button = ttk.Button(house, text = "Next: Transport", command = selec2).grid(row = 2, column = 1, pady = 10)
 
-bus_label.grid(row=0, column=0)
+bus_label = tk.Label(transport, text = "Bus (in km):", padx = 50, pady =10).grid(row=0, column=0)
+bus_entry = ttk.Entry(transport)
 bus_entry.grid(row = 0, column=1)
 
-local_train_label = tk.Label(tab2, text = "Local Train (in km):", padx = 50, pady =10)
-local_train_entry = ttk.Entry(tab2)
-
-local_train_label.grid(row=1, column=0)
+local_train_label = tk.Label(transport, text = "Local Train (in km):", padx = 50, pady =10).grid(row=1, column=0)
+local_train_entry = ttk.Entry(transport)
 local_train_entry.grid(row = 1, column=1)
 
-train_label = tk.Label(tab2, text = "Train (in km):", padx = 50, pady =10)
-train_entry = ttk.Entry(tab2)
-
-train_label.grid(row=2, column=0)
+train_label = tk.Label(transport, text = "Train (in km):", padx = 50, pady =10).grid(row=2, column=0)
+train_entry = ttk.Entry(transport)
 train_entry.grid(row = 2, column=1)
 
-elec_button = ttk.Button(tab2, text = "Back: House", command = selec0)
-elec_button.grid(row = 3, column = 0, pady = 10)
+house_button = ttk.Button(transport, text = "Back: House", command = selec1).grid(row = 3, column = 0, pady = 10)
+result_button = ttk.Button(transport, text = "Next: Result", command = selec3).grid(row = 3, column = 1, pady = 10)
 
-result_button = ttk.Button(tab2, text = "Next: Result", command = selec2)
-result_button.grid(row = 3, column = 1, pady = 10)
-
-final_button = ttk.Button(root, text = "Calculate CFC", command = button_pressed)
-final_button.pack(pady = 10)
+result_text = tk.StringVar()
+result_text.set("Enter the values to calculate your Carbon footprint")
+result_message = tk.Label(result, textvariable = result_text).grid(row = 1, column = 1, padx = 50, pady = 10)
 
 final_text = tk.StringVar()
 final_text.set("Calculate Carbon Footprint")
-final_message = tk.Label(root, textvariable = final_text)
-final_message.pack(pady = 10)
+final_message = tk.Label(result, textvariable = final_text).grid(row = 0, column = 1, padx = 50, pady = 10)
+
+final_button = ttk.Button(root, text = "Calculate CarbonFootprint", command = button_pressed).pack(pady = 20)
 
 root.mainloop()
